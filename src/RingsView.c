6d6e85a89bb3f26b368c13df7a1bd17f6d41d7b3
Xavier JAFFREZIC <xavier.jaffrezic@gmail.com>
2015-07-16 22:08:20 +0200

    added file for drawing rings in Update Procedure callback not in previous

---
+++ src/RingsView.c
@@ -0,0 +1,25 @@
+//#include <RingsView.h>
+#include <pebble.h>
+
+
+// Called Function for rendering Background
+static void DrawRings(Layer *SelectedLayer, GContext* GraphicContext)
+{
+  GRect Bounds = layer_get_bounds(SelectedLayer);
+  uint16_t Radius = Bounds.size.w / 3;
+  uint8_t Width = Radius / 10;	
+  GPoint Center = GPoint( Bounds.size.w / 2, Bounds.size.h /2);	
+
+  graphics_context_set_stroke_width(GraphicContext, Width);
+
+  graphics_context_set_stroke_color(GraphicContext, GColorWindsorTan); 
+  graphics_draw_circle(GraphicContext, Center, Radius);		
+
+  graphics_context_set_stroke_color(GraphicContext, GColorVeryLightBlue);
+  graphics_draw_circle(GraphicContext, Center, Radius+5);		
+
+  graphics_context_set_stroke_color(GraphicContext, GColorIslamicGreen);
+  graphics_draw_circle(GraphicContext, Center, Radius-5);		
+
+}
+
