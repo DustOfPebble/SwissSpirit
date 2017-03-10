910fcdb5d9b1181a18ca5cde47b24e012c8f3ad0
Xavier <xavier@jaffrezic.net>
2017-03-10 17:02:16 +0100

    New SVG design applied

--- src/BatteryView.c
+++ src/BatteryView.c
@@ -5,27 +5,31 @@
 
 #include "BatteryView.h"
 //#################################################################################
-int SelectedIndicator;
-int MaxIndicator;
-GDrawCommandSequence *IndicatorCollection;
-GPoint BatteryIconOrigin;
+int index;
+int maxIndex;
+GDrawCommandSequence *Levels;
+GDrawCommandImage *Container;
+
+GPoint Origin;
 //#################################################################################
 void initLayoutBattery() {
 	GRect Bounds = layer_get_bounds(batteryDisplay);
-	IndicatorCollection = gdraw_command_sequence_create_with_resource(RESOURCE_ID_BATTERY_POWER);
-	GSize BatteryBox = gdraw_command_sequence_get_bounds_size(IndicatorCollection);
-	BatteryIconOrigin = GPoint((Bounds.size.w - BatteryBox.w)/2,(Bounds.size.h - BatteryBox.h)/2);
-	MaxIndicator = gdraw_command_sequence_get_num_frames(IndicatorCollection);
+	Container = gdraw_command_image_create_with_resource(RESOURCE_ID_BATTERY);
+	GSize BatteryBox = gdraw_command_image_get_bounds_size(Container);
+	Origin = GPoint((Bounds.size.w - BatteryBox.w)/2,(Bounds.size.h - BatteryBox.h)/2);
+	Levels = gdraw_command_sequence_create_with_resource(RESOURCE_ID_BATTERY_LEVELS);
+	maxIndex = gdraw_command_sequence_get_num_frames(Levels);
 }
 //#################################################################################
 void updateBattery(BatteryChargeState batteryInfos) {
-	SelectedIndicator = batteryInfos.charge_percent / 10;
-	if (SelectedIndicator > MaxIndicator)  SelectedIndicator = MaxIndicator;
+	index = batteryInfos.charge_percent / 10;
+	if (index > maxIndex)  index = maxIndex;
 	layer_mark_dirty(batteryDisplay);
 }
 //#################################################################################
-void drawBattery(Layer *frame, GContext* context) {	  
-   GDrawCommandFrame *Indicator = gdraw_command_sequence_get_frame_by_index(IndicatorCollection, SelectedIndicator);
-   gdraw_command_frame_draw(context, IndicatorCollection, Indicator, BatteryIconOrigin); 
+void drawBattery(Layer *frame, GContext* context) {
+   GDrawCommandFrame *Level = gdraw_command_sequence_get_frame_by_index(Levels, index);
+   gdraw_command_frame_draw(context, Levels, Level, Origin);
+   gdraw_command_image_draw(context, Container, Origin);
 }
 //#################################################################################
--- src/Globals.h
+++ src/Globals.h
@@ -4,8 +4,6 @@
 extern GDrawCommandImage *icon_heart_beat;
 extern GDrawCommandImage *icon_calls_missed;
 extern GDrawCommandImage *icon_time_elapsed;
-extern GDrawCommandImage *icon_phone_lost;
-extern GBitmap *icon_phone_linked;
 
 extern TextLayer *timeDisplay;
 extern TextLayer *dateDisplay;
