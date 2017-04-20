08aca5980d54c58024b91c05f59e065304d1b55e
Xavier <xavier@jaffrezic.net>
2017-04-20 08:57:13 +0200

    ViewSelector code refactoring

--- src/c/ViewSelector.c
+++ src/c/ViewSelector.c
@@ -1,42 +1,51 @@
 #include "ViewSelector.h"
-
+//############################################################################
+void showWeather() {
+	layer_set_hidden(heartDisplay,true);
+	layer_set_hidden(phoneDisplay,true);
+	layer_set_hidden(weatherDisplay,false);
+}
+//############################################################################
+void showPhoneLink() {
+	layer_set_hidden(heartDisplay,true);
+	layer_set_hidden(weatherDisplay,true);
+	layer_set_hidden(phoneDisplay,false);
+}
+//############################################################################
+void showHeartSensor() {
+	layer_set_hidden(weatherDisplay,true);
+	layer_set_hidden(phoneDisplay,true);
+	layer_set_hidden(heartDisplay,false);
+}
+//############################################################################
 void updateViewSelector(){
 
 	/****************************************************************************************************
 	 *  Business logic :
 	 *  ================
-	 * 0: Phone disconnected --> Highest Priority 
+	 * 0: Phone disconnected --> Highest Priority
 	 * 1: Heart Sensor --> Sensor lost : Wait 30 secondes => Lower priority screen
-	 * 2: Weather --> No Weather update => Lower priority screen 
+	 * 2: Weather --> No Weather update => Lower priority screen
 	 * 3: Phone Connected --> [Calls/Messages]
 	 ******************************************************************************************************/
 
 	// Disconnection ==> Higest priority
 	if (SecondsSinceDisconnection > 0)	{
-		layer_set_hidden(heartDisplay,true);
-		layer_set_hidden(weatherDisplay,true);
-		layer_set_hidden(phoneDisplay,false);
+		showPhoneLink();
 		return;
 	}
-	
-	// Heart Sensor connected ==> High priority 
+
+	// Heart Sensor connected ==> High priority
 	if (SecondsSinceSensorUpdate < 30) {
-		// Phone Connected ==> 
+		// Phone Connected ==>
 		if (SecondsSinceConnection < 15) {
-			layer_set_hidden(weatherDisplay,true);
-			layer_set_hidden(phoneDisplay,true);
-			layer_set_hidden(heartDisplay,false);
+			showHeartSensor();
 			return;
 		}
-		layer_set_hidden(heartDisplay,true);
-		layer_set_hidden(phoneDisplay,true);
-		layer_set_hidden(weatherDisplay,false);
+		showWeather();
 		return;
 	}
 
-
 	// Fallback mode
-	layer_set_hidden(heartDisplay,true);
-	layer_set_hidden(phoneDisplay,true);
-	layer_set_hidden(weatherDisplay,false);
+	showWeather();
 }
--- src/c/ViewSelector.h
+++ src/c/ViewSelector.h
@@ -1,5 +1,7 @@
 #include <pebble.h>
 #include "Globals.h"
 
-
+void showWeather();
+void showPhoneLink();
+void showHeartSensor();
 void updateViewSelector();
